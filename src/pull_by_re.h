55a27fa7b194017599f0edd5416026a8b812b6be
Nilesh Patra <nilesh@nileshpatra.info>
2024-03-15 02:36:59 +0530

    Fixup implicit-function-declaration warnings

--- src/pull_by_re.h
+++ src/pull_by_re.h
@@ -1,5 +1,5 @@
-#ifndef PULL_BY_SIZE_H
-#define PULL_BY_SIZE_H
+#ifndef PULL_BY_RE_H
+#define PULL_BY_RE_H
 
 int pull_by_re(char *input_file, char *aStrRegex, int min, int max, int length, int exclude, int convert, int just_count);
 
